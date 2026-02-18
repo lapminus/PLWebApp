d50e29921cfcf4768ec737bc44a91e68156d4e02
lapminus <cyoung35@calstatela.edu>
2026-02-18 11:37:46 -0800

    Update CorsConfig with Vercel url

--- backend/src/main/java/com/learning/pl/config/CorsConfig.java
+++ backend/src/main/java/com/learning/pl/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200, https://pl-web-app-ten.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
